dba15c4e4f57607d8ad87d665d58ec1b36e7ce3a
Newaz <newazbenalam@gmail.com>
2022-06-17 16:33:18 +0600

    Render first template(s)

--- src/server_django/settings.py
+++ src/server_django/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mduhc1ga_p5!jpo$c8n8d+f#z4th@x)ptzymg&0%wh6*lw%!hm'
+SECRET_KEY = 'django-insecure-+=my8(_q836hm#%!f-c)gk2col95cws!y-0dt^(c5h8**$+h*l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'server_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
